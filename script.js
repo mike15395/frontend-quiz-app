eea174b4bfeaaa61bbad6405410afdb6025797fc
mike15395 <mikhildesai@gmail.com>
2025-02-22 16:18:05 +0530

    small-bug-fix-img-alt

--- script.js
+++ script.js
@@ -108,7 +108,8 @@ function displayQuizData(data) {
         if (item.title == e.target.id) {
           hideIntialQuizPage();
           currentHeading.innerHTML =
-            `<img src=${item.icon}  id=${item.title}>` + item.title;
+            `<img src=${item.icon}  id=${item.title}> alt="icon-heading"` +
+            item.title;
           displayQuestions(item["questions"], 0, item);
         }
       });
